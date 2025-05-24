0d64ca4119b035992c8da32a14f16f51c4b55d3a
nothings0 <nhanphamx3@gmail.com>
2025-05-24 12:53:18 +0700

    update logo 4

--- src/pages/activeAccount/ActiveAccount.jsx
+++ src/pages/activeAccount/ActiveAccount.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { ActiveAccount } from "../../redux/apiRequest";
-const logo = require("../../assets/logo.png");
+import logo from "../../assets/logo.png"
 const ActiveAccountPage = () => {
   const { slug } = useParams();
   const [msg, setMsg] = useState("");
